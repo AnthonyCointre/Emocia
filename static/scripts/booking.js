4ab6ca28665f2508c6f9287a5440528860456298
AnthonyCointre <8732@holbertonstudents.com>
2026-07-02 19:52:07 +0200

    delete files

--- static/scripts/booking.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const errorMessage = document.getElementById("booking-error");
-
-  if (errorMessage && errorMessage.value) {
-    alert(errorMessage.value);
-  }
-});
--- tests/appointments_db/delete_appointments_db.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_path = "data/appointments.db"
-
-if os.path.exists(file_path):
-    os.remove(file_path)
-    print("appointments.db deleted successfully.")
-else:
-    print("appointments.db doesn't exist.")
