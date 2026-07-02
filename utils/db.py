eb18de95537a4bfab9bacf766e0850deb4b9148c
AnthonyCointre <8732@holbertonstudents.com>
2026-07-02 19:56:17 +0200

    update controllers and utils

--- utils/db.py
+++ utils/db.py
@@ -1,15 +1,21 @@
-from dotenv import load_dotenv
 import os
 import sqlite3
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-def get_db_connection(db_env_var):
+def get_db_connection(db_env_var="DATABASE_URL"):
     db_url = os.getenv(db_env_var)
-    if db_url is None:
+
+    if not db_url:
         raise ValueError(f"Environment variable '{db_env_var}' not set")
+
     if db_url.startswith("sqlite:///"):
         db_path = db_url.replace("sqlite:///", "")
-        return sqlite3.connect(db_path)
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        conn.execute("PRAGMA foreign_keys = ON;")
+        return conn
+
     raise ValueError("Unsupported database URL")
