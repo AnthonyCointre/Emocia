8e367fc5a05d0e62932b00a3c1a63f1ffb2a391f
AnthonyCointre <8732@holbertonstudents.com>
2026-07-03 19:19:20 +0200

    update app

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_cors import CORS
+from config import Config
+from extensions import db
 from routes.main_routes import main_bp
 from routes.booking_routes import booking_bp
 from routes.auth_routes import auth_bp
@@ -10,7 +12,13 @@ from routes.admin_routes import admin_bp
 load_dotenv()
 
 app = Flask(__name__)
+
 app.secret_key = os.getenv("SECRET_KEY")
+
+app.config.from_object(Config)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 CORS(app, origins=[
     "http://localhost:5000",
@@ -22,5 +30,5 @@ app.register_blueprint(booking_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(admin_bp)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
