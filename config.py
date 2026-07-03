14a7aabbbf1fb83098aa2924642263ba981791e9
AnthonyCointre <8732@holbertonstudents.com>
2026-07-03 19:17:01 +0200

    create config, extensions and models

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent
+
+db_url = os.getenv("DATABASE_URL")
+
+if db_url.startswith("sqlite:///"):
+    db_path = BASE_DIR / db_url.replace("sqlite:///", "")
+    db_url = f"sqlite:///{db_path}"
+
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = db_url
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
